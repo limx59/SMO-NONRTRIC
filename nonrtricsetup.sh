c08cf0ec1c3f04d8068c2f7b4e71bda5434aa7bb
Mingxuan Li <limx59@126.com>
2024-05-06 16:43:04 -0700

    Update nonrtricsetup.sh

--- nonrtricsetup.sh
+++ nonrtricsetup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo docker-compose --env-file .env -f docker-compose.yaml -f control-panel/docker-compose.yaml -f nonrtric-gateway/docker-compose.yaml -f policy-service/docker-compose.yaml -f ics/docker-compose.yaml -f a1-sim/docker-compose.yaml up -d
+sudo docker compose --env-file .env -f docker-compose.yaml -f control-panel/docker-compose.yaml -f nonrtric-gateway/docker-compose.yaml -f policy-service/docker-compose.yaml -f ics/docker-compose.yaml -f a1-sim/docker-compose.yaml up -d

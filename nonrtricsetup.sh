a3d5581cf2cfb5e699821dc2bcb85748b7b36193
Mingxuan Li <limx59@126.com>
2024-05-07 20:09:02 -0700

    Update nonrtricsetup.sh

--- nonrtricsetup.sh
+++ nonrtricsetup.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-sudo docker compose --env-file .env -f docker-compose.yaml -f control-panel/docker-compose.yaml -f nonrtric-gateway/docker-compose.yaml -f policy-service/docker-compose.yaml -f ics/docker-compose.yaml -f a1-sim/docker-compose.yaml up -d
+# sudo docker compose --env-file .env -f docker-compose.yaml -f control-panel/docker-compose.yaml -f nonrtric-gateway/docker-compose.yaml -f policy-service/docker-compose.yaml -f ics/docker-compose.yaml -f a1-sim/docker-compose.yaml up -d
+
+sudo docker compose --env-file .env -f docker-compose.yaml -f control-panel/docker-compose.yaml -f nonrtric-gateway/docker-compose.yaml -f policy-service/docker-compose.yaml -f ics/docker-compose.yaml -f a1-sim/docker-compose.yaml -f dmaap-mediator-go/docker-compose.yaml -f dmaap-mediator-java/docker-compose.yaml -f mr/docker-compose.yml -f rapp/docker-compose.yaml  -f sdnc/docker-compose.yml up -d
+cd docker-compose-policy-framework
+chmod +x wait_for_port.sh
+sudo docker compose up -d
